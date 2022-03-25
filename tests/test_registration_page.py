1f6c96a805147e5c9a4c4f1ed2cd2c8626fe02aa
Aleksandr <Ivanovsanya5@mail.ru>
2022-03-25 12:44:52 +0300

    add [200~

---
+++ tests/test_registration_page.py
@@ -0,0 +1,26 @@
+from page.registration_page import RegistrationPage
+import pytest
+import random
+import time
+
+
+def test_guest_see_registration_page(driver):             # проверка перехода на страницу регистрации и веб-элементов
+    link = "http://demowebshop.tricentis.com/"
+    page = RegistrationPage(driver, link)
+    page.open()
+    page.should_be_registration_page()
+
+
+def test_registration_new_user(driver):                   # регистрация нового пользователя
+    link = "http://demowebshop.tricentis.com/register"
+    page = RegistrationPage(driver, link)
+    page.open()
+    count = random.randint(1, 100)
+    first_name = str("Anton") + str(count)
+    last_name = str("Arnoldov") + str(count)
+    email = str(time.time()) + "@fakemail.org"
+    password = str(time.time() + count)
+    confirm_password = password
+    page.register_new_user(first_name, last_name, email, password, confirm_password)
+
+
