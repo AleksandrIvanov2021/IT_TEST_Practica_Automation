e5c85ebd3baf1d24ca6905575df96abb93dffd97
Aleksandr <Ivanovsanya5@mail.ru>
2022-03-25 12:35:08 +0300

    add class BasePage

---
+++ page/BasePage.py
@@ -0,0 +1,19 @@
+from selenium.common.exceptions import NoSuchElementException
+from page.locators import BasePageLocators
+
+
+class BasePage:
+    def __init__(self, driver, url, timeout=10):
+        self.driver = driver
+        self.url = url
+        self.driver.implicitly_wait(timeout)
+
+    def open(self):
+        self.driver.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
