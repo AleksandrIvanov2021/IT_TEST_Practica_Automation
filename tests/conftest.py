d76e20ebecb2917acfd93da709ce8aa7a07cfcec
Aleksandr <Ivanovsanya5@mail.ru>
2022-03-30 17:19:09 +0300

    add pytest_addoption, update driver

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,31 +1,40 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options as chrome_options
+from selenium.webdriver.chrome.options import Options
 
 
-@pytest.fixture
-def get_chrome_options():
-    options = chrome_options()
-    options.add_argument("chrome")
-    options.add_argument("--start-maximized")
-    options.add_argument("--window-size=1280,720")
-    return options
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+    parser.addoption("--headless", action="store_true", help="Run driver in headless mode.")
 
 
-@pytest.fixture
-def get_webdriver(get_chrome_options):
-    options = get_chrome_options
-    driver = webdriver.Chrome(options=options)
-    return driver
-
-
-@pytest.fixture(scope='function')
-def driver(request, get_webdriver):
-    driver = get_webdriver
-    if request.cls is not None:
-        request.cls.driver = driver
+@pytest.fixture(scope="function")
+def driver(request):
+    browser_name = request.config.getoption("browser_name")
+    driver = None
+    if browser_name == "chrome":
+        if not request.config.getoption("--headless"):
+            print("\nstart chrome browser for test..")
+            options = Options()
+            options.add_argument("--window-size=1280,720")
+            driver = webdriver.Chrome(options=options)
+        else:
+            print("\nstart chrome browser for test..")
+            options = Options()
+            options.add_argument("--headless")
+            options.add_argument("--window-size=1280,720")
+            driver = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        driver = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield driver
+    print("\nquit browser..")
     driver.quit()
+
+
 
 
 
