6b42c9a783993f383b176c310c30372e8a52cb67
Aleksandr <Ivanovsanya5@mail.ru>
2022-03-30 17:01:46 +0300

    add class MainPageLocators

--- page/locators.py
+++ page/locators.py
@@ -1,8 +1,14 @@
 from selenium.webdriver.common.by import By
 
 
-class RegistrationPageLocators:
+class MainPageLocators:
     REGISTRATION_URL = (By.CSS_SELECTOR, ".ico-register")
+    LOGIN_URL = (By.CSS_SELECTOR, ".ico-login")
+    ACCOUNT_LINK = (By.CSS_SELECTOR, ".header-links>ul>li>a.account")
+    CHANGE_PASSWORD_LINK = (By.CSS_SELECTOR, ".list>li:nth-child(7)>a")
+
+
+class RegistrationPageLocators:
     PERSONAL_DETAILS_FORM = (By.CSS_SELECTOR, ".fieldset:nth-child(2)")
     PASSWORD_FORM = (By.CSS_SELECTOR, ".fieldset:nth-child(3)")
     GENDER_MALE_RADIOBUTTON = (By.CSS_SELECTOR, "#gender-male")
@@ -19,7 +25,6 @@ class RegistrationPageLocators:
 
 
 class LoginPageLocators:
-    LOGIN_URL = (By.CSS_SELECTOR, ".ico-login")
     RETURNING_CUSTOMER_FORM = (By.CSS_SELECTOR, ".returning-wrapper")
     LOGIN_EMAIL = (By.CSS_SELECTOR, "#Email")
     LOGIN_PASSWORD = (By.CSS_SELECTOR, "#Password")
@@ -33,8 +38,6 @@ class LoginPageLocators:
 
 
 class ChangePasswordPageLocators:
-    ACCOUNT_LINK = (By.CSS_SELECTOR, ".header-links>ul>li>a.account")
-    CHANGE_PASSWORD_LINK = (By.CSS_SELECTOR, ".list>li:nth-child(7)>a")
     CHANGE_PASSWORD_FORM = (By.CSS_SELECTOR, ".fieldset")
     OLD_PASSWORD = (By.CSS_SELECTOR, "#OldPassword")
     NEW_PASSWORD = (By.CSS_SELECTOR, "#NewPassword")
