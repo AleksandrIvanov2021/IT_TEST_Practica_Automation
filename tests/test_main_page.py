9dd3d497c3c71a69f46234ccbcb732a91120c9e7
Aleksandr <Ivanovsanya5@mail.ru>
2022-03-31 10:40:26 +0300

    update test_guest_see_main_page

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -5,18 +5,18 @@ from page.main_page import MainPage
 
 @pytest.mark.critical_tests
 def test_guest_see_main_page(driver):  # Проверка элементов на главной странице (Registr,Login,Account,ChangePassword)
-    link = "http://demowebshop.tricentis.com/"
-    page = MainPage(driver, link)
+    url = "http://demowebshop.tricentis.com/"
+    page = MainPage(driver, url)
     page.open()
     page.should_be_main_page()
-    link = 'http://demowebshop.tricentis.com/login'
-    page = LoginPage(driver, link)
+    url = 'http://demowebshop.tricentis.com/login'
+    page = LoginPage(driver, url)
     page.open()
     email = 'Tokar@mail.com'
     password = '123456'
     page.should_be_authorized_user(email, password)
-    link = "http://demowebshop.tricentis.com/"
-    page = MainPage(driver, link)
+    url = "http://demowebshop.tricentis.com/"
+    page = MainPage(driver, url)
     page.open()
     page.should_be_main_page_autorization()
 
