b165e6fa642b59b297ba90cc46facf6e4ca21020
Aleksandr <Ivanovsanya5@mail.ru>
2022-04-04 15:42:26 +0300

    update change_password, change_incorrect_old_password, add 5 methods

--- page/change_password_page.py
+++ page/change_password_page.py
@@ -25,14 +25,38 @@ class ChangePasswordPage(BasePage):
     def __should_be_change_password_button(self):
         assert self.is_element_present(*ChangePasswordPageLocators.CHANGE_PASSWORD_BUTTON), "Button is not presented"
 
-    def change_incorrect_old_password(self, old_password, new_password, confirm_password):
+    def filling_old_password_changed(self, old_password):
         old_password_field = self.driver.find_element(*ChangePasswordPageLocators.OLD_PASSWORD)
         old_password_field.send_keys(old_password)
+
+    def filling_new_password_changed(self, new_password):
         new_password_field = self.driver.find_element(*ChangePasswordPageLocators.NEW_PASSWORD)
         new_password_field.send_keys(new_password)
+
+    def filling_confirm_password_changed(self, confirm_password):
         confirm_password_field = self.driver.find_element(*ChangePasswordPageLocators.CONFIRM_PASSWORD)
         confirm_password_field.send_keys(confirm_password)
+
+    def click_to_the_change_password_button(self):
         change_password_button = self.driver.find_element(*ChangePasswordPageLocators.CHANGE_PASSWORD_BUTTON)
         change_password_button.click()
+
+    def checking_the_password_change_message(self):
+        message = self.driver.find_element(*ChangePasswordPageLocators.CHANGE_MESSAGE).text
+        assert message == 'Password was changed', 'Error when changing password'
+
+    def checking_the_incorrect_old_password_message(self):
         change_message = self.driver.find_element(*ChangePasswordPageLocators.CHANGE_MESSAGE_INCORRECT).text
         assert change_message == "Old password doesn't match"
+
+    def change_password(self, old_password, new_password, confirm_password):
+        self.filling_old_password_changed(old_password)
+        self.filling_new_password_changed(new_password)
+        self.filling_confirm_password_changed(confirm_password)
+        self.click_to_the_change_password_button()
+
+    def change_incorrect_old_password(self, old_password, new_password, confirm_password):
+        self.filling_old_password_changed(old_password)
+        self.filling_new_password_changed(new_password)
+        self.filling_confirm_password_changed(confirm_password)
+        self.click_to_the_change_password_button()
