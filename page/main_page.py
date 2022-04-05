335540cd2ea2998bae612827a3d1816ff9e90f31
Aleksandr <Ivanovsanya5@mail.ru>
2022-04-05 19:18:11 +0300

    update def should_be_main_page

--- page/main_page.py
+++ page/main_page.py
@@ -6,6 +6,8 @@ class MainPage(BasePage):
     def should_be_main_page(self):
         self.should_be_login_url()
         self.should_be_registration_url()
+        self.should_be_product_page_books_link()
+        self.should_be_basket_link()
 
     def should_be_main_page_autorization(self):
         self.should_be_account_url()
