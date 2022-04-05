7b1c97dc3de198dc27a69edfdfa3687d469a1940
Aleksandr <Ivanovsanya5@mail.ru>
2022-04-05 19:10:25 +0300

    update MainPageLocators, add ProductPage and BasketPage Locators

--- page/locators.py
+++ page/locators.py
@@ -6,6 +6,8 @@ class MainPageLocators:
     LOGIN_URL = (By.CSS_SELECTOR, ".ico-login")
     ACCOUNT_LINK = (By.CSS_SELECTOR, ".header-links>ul>li>a.account")
     CHANGE_PASSWORD_LINK = (By.CSS_SELECTOR, ".list>li:nth-child(7)>a")
+    PRODUCT_PAGE_BOOKS_LINK = (By.CSS_SELECTOR, ".inactive>a")
+    BASKET_LINK = (By.CSS_SELECTOR, ".ico-cart>span.cart-label")
 
 
 class RegistrationPageLocators:
@@ -45,3 +47,28 @@ class ChangePasswordPageLocators:
     CHANGE_PASSWORD_BUTTON = (By.CSS_SELECTOR, ".button-1.change-password-button")
     CHANGE_MESSAGE = (By.CSS_SELECTOR, ".result")
     CHANGE_MESSAGE_INCORRECT = (By.CSS_SELECTOR, ".validation-summary-errors>ul>li")
+
+
+class ProductPageLocators:
+    BOOK_NAME_DETAIL_PRODUCT_PAGE = (By.CSS_SELECTOR, ".product-name>h1")
+    BOOK_PRICE_DETAIL_PRODUCT_PAGE = (By.CSS_SELECTOR, ".price-value-22")
+    ADD_BUTTON_DETAIL_PRODUCT_PAGE = (By.CSS_SELECTOR, "#add-to-cart-button-22[value='Add to cart']")
+    ADD_BUTTON_LIST_PRODUCT_PAGE = (By.CSS_SELECTOR, ".button-2.product-box-add-to-cart-button[onclick*='22/1/1']")
+    SUCCESS_MESSAGE_ADD_PRODUCT_TO_BASKET = (By.CSS_SELECTOR, ".content>a")
+
+
+class BasketPageLocators:
+    BOOK_NAME_BASKET_PAGE = (By.CSS_SELECTOR, ".product-name")
+    BOOK_PRICE_BASKET_PAGE = (By.CSS_SELECTOR, ".product-unit-price")
+    COUNT_PRODUCT_FIELD = (By.CSS_SELECTOR, ".qty-input")
+    TOTAL_PRICE_PRODUCT = (By.CSS_SELECTOR, ".product-subtotal")
+    CHECKBOX_AGREEMENT = (By.CSS_SELECTOR, ".terms-of-service #termsofservice")
+    BUTTON_CHECKOUT = (By.CSS_SELECTOR, "#checkout[name='checkout']")
+    ORDERING_COUNTRY = (By.CSS_SELECTOR, "#BillingNewAddress_CountryId.valid")
+    ORDERING_CITY = (By.CSS_SELECTOR, "#BillingNewAddress_City ")
+    ORDERING_ADDRESS = (By.CSS_SELECTOR, "#BillingNewAddress_Address1")
+    ORDERING_POSTAL_CODE = (By.CSS_SELECTOR, "#BillingNewAddress_ZipPostalCode")
+    ORDERING_PHONE = (By.CSS_SELECTOR, "#BillingNewAddress_PhoneNumber")
+    ORDERING_BUTTON_CONTINUE = (By.CSS_SELECTOR, "#billing-buttons-container>input")
+
+
