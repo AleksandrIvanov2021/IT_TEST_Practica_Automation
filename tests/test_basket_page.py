cf35f230cf1cce121e55f099e89ea8f43519e828
Aleksandr <Ivanovsanya5@mail.ru>
2022-04-05 19:26:46 +0300

    add guest_see_basket_page, quantity_change_recalculation, ordering

---
+++ tests/test_basket_page.py
@@ -0,0 +1,49 @@
+from page.basket_page import BasketPage
+from page.login_page import LoginPage
+import time
+
+
+def test_guest_see_basket_page(driver):         # проверка элеменов в корзине (предварительно добавив в нее товар)
+    url = "http://demowebshop.tricentis.com/health"
+    page = BasketPage(driver, url)
+    page.open()
+    page.add_to_basket_detail_product_page()
+    time.sleep(1)
+    page.enter_to_basket()
+    page.should_be_basket_page()
+    time.sleep(1)
+
+
+def test_quantity_change_recalculation(driver):         # тест на изменение количества товара в корзине
+    url = "http://demowebshop.tricentis.com/health"     # (проверка перерасчета суммы после изменения количества)
+    page = BasketPage(driver, url)
+    page.open()
+    page.add_to_basket_detail_product_page()
+    time.sleep(1)
+    page.enter_to_basket()
+    count = 5
+    page.quantity_change(count)
+    time.sleep(2)
+
+
+def test_ordering(driver):
+    url = 'http://demowebshop.tricentis.com/login'
+    page = LoginPage(driver, url)
+    page.open()
+    email = 'Tokar@mail.com'
+    password = '123456'
+    page.authorized_user(email, password)
+    url = "http://demowebshop.tricentis.com/health"
+    page = BasketPage(driver, url)
+    page.open()
+    page.add_to_basket_detail_product_page()
+    page.enter_to_basket()
+    time.sleep(2)
+    city = 'Санкт-Петербург'
+    address = 'Невский проспект 23'
+    postal_code = '190000'
+    phone = '8-900-777-55-55'
+    page.ordering(city, address, postal_code, phone)
+    time.sleep(2)
+
+
